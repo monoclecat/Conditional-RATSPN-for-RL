c13a5a2b3d40b879344a385d360a1f290ab48977
Andrew DeLay <git@andrewdelay.com>
2022-08-16 14:03:23 +0200

    Separated 2d entropy experiment and plotting.

---
+++ experiments/2d_entropy.py
@@ -0,0 +1,158 @@
+import numpy as np
+from scipy.stats import multivariate_normal as normal_pdf
+import torch as th
+import matplotlib.pyplot as plt
+from rat_spn import RatSpn, RatSpnConfig
+from distributions import RatNormal
+from experiments.train_mnist import count_params
+from experiments.train_cspn_mnist_gen import time_delta
+from torch import optim
+import torch.distributions as dist
+import gif
+import os
+import csv
+import time
+from utils import *
+from tqdm import tqdm
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp_name', '-name', type=str, default='vips_gmm_test',
+                        help='Experiment name. The results dir will contain it.')
+    parser.add_argument('--seed', '-s', type=int, nargs='+', required=True)
+    parser.add_argument('--steps', type=int, default=10000)
+    parser.add_argument('--device', '-d', type=str, default='cuda', help='cuda or cpu')
+    parser.add_argument('--ent_approx_sample_size', '-samples', type=int, default=5)
+    parser.add_argument('--results_dir', type=str, default='../../spn_experiments',
+                        help='The base directory where the directory containing the results will be saved to.')
+    parser.add_argument('--additional_grad', action='store_true',
+                        help="If True, additional gradients are used (different for each method).")
+    parser.add_argument('--log_interval', '-log', type=int, default=1000)
+    parser.add_argument('--max_abs_mean', type=int, default=50)
+    parser.add_argument('--vi', action='store_true', help="Approximate with VI")
+    parser.add_argument('--huber', action='store_true', help="Use Huber lower bound")
+    parser.add_argument('--montecarlo', action='store_true', help="Approximate entropy with samples of the root")
+    parser.add_argument('--layerwise', action='store_true', help="Use layerwise entropy approximation")
+    parser.add_argument('--repetitions', '-R', type=int, default=3, help='Number of parallel CSPNs to learn at once. ')
+    parser.add_argument('--features', '-F', type=int, default=2, help='Number of features in the leaf layer')
+    parser.add_argument('--num_dist', '-I', type=int, default=4, help='Number of Gauss dists per pixel.')
+    parser.add_argument('--num_sums', '-S', type=int, default=3, help='Number of sums per RV in each sum layer.')
+    args = parser.parse_args()
+    assert args.huber + args.montecarlo + args.vi == 1
+
+    for d in [args.results_dir]:
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+    probs = None
+    num_dimensions = 2
+    num_true_components = 10
+
+    min_x = -args.max_abs_mean
+    max_x = -min_x
+
+    for seed in args.seed:
+        th.manual_seed(seed)
+
+        load_path = None
+        if load_path is None:
+            config = RatSpnConfig()
+            config.C = 1
+            config.F = args.features
+            config.R = args.repetitions
+            config.D = int(np.log2(config.F))
+            config.I = args.num_dist
+            config.S = args.num_sums
+            config.dropout = 0.0
+            config.leaf_base_class = RatNormal
+            config.leaf_base_kwargs = {'min_mean': float(min_x+1), 'max_mean': float(max_x-1)}
+            model = RatSpn(config).to(args.device)
+            count_params(model)
+        else:
+            model = th.load(load_path, map_location=args.device)
+            # Only for high_ent_ratspn.pt
+            w = model.layer_index_to_obj(model.max_layer_index).weight_param.data
+            w = w.unsqueeze(0)
+            model.layer_index_to_obj(model.max_layer_index).weight_param = th.nn.Parameter(w)
+
+        optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+        n_steps = args.steps
+        if args.huber:
+            exp_name = f"entmax_huberLB" \
+                       f"_seed{seed}" \
+                       f"_{n_steps}steps" \
+                       f"_{args.features}feat"
+        elif args.montecarlo:
+            exp_name = f"entmax_MCapprox" \
+                       f"_{args.ent_approx_sample_size}samples" \
+                       f"{'_sampledwithgrad' if args.additional_grad else ''}" \
+                       f"_seed{seed}" \
+                       f"_{n_steps}steps" \
+                       f"_{args.features}feat"
+        else:
+            exp_name = f"entmax_VIapprox" \
+                       f"{'_layerwise' if args.layerwise else ''}" \
+                       f"_{args.ent_approx_sample_size}samples" \
+                       f"{'_gradthruresp' if args.additional_grad else ''}" \
+                       f"_seed{seed}" \
+                       f"_{n_steps}steps" \
+                       f"_{args.features}feat"
+        save_path = os.path.join(args.results_dir, f"{exp_name}")
+        experiment_nr = 0
+        while os.path.exists(save_path):
+            experiment_nr += 1
+            save_path = os.path.join(args.results_dir, f"{exp_name}__{experiment_nr}")
+        os.makedirs(save_path, exist_ok=False)
+        model_save_path = os.path.join(save_path, "models")
+        os.makedirs(model_save_path, exist_ok=False)
+        file_name_base = f"{exp_name}__{experiment_nr}"
+        print(f"Running for {n_steps} steps, saving model every {args.log_interval} steps.")
+
+        def bookmark():
+            pass
+        losses = []
+        t_start = time.time()
+        plot_at = 150
+
+        def verbose_callback(step):
+            return False
+            # return step == plot_at
+            # return True
+
+        # th.set_anomaly_enabled(True)
+        ent_args = {
+            'sample_size': args.ent_approx_sample_size,
+            'grad_thru_resp': args.additional_grad,
+            'verbose': True,
+        }
+
+        with open(os.path.join(save_path, f"config_{exp_name}.csv"), 'w') as f:
+            w = csv.DictWriter(f, vars(args).keys())
+            w.writeheader()
+            w.writerow(vars(args))
+
+        for step in tqdm(range(int(n_steps)), desc='Progress'):
+            if step % args.log_interval == 0:
+                th.save(model, os.path.join(model_save_path, f"{file_name_base}_step{step:06d}.pt"))
+
+            if args.vi:
+                ent, log = model.vi_entropy_approx_layerwise(**ent_args)
+            elif args.huber:
+                ent, log = model.huber_entropy_lb(verbose=True)
+            elif args.montecarlo:
+                ent = model.monte_carlo_ent_approx(
+                    sample_size=args.ent_approx_sample_size, sample_with_grad=args.additional_grad,
+                )
+            else:
+                raise Exception("No entropy calculation mode was selected!")
+                # ent, log = model.vi_entropy_approx(**ent_args)
+            loss = -ent.mean()
+            losses.append(loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f"Finished with seed {seed}.")
+
---
+++ experiments/2d_entropy_plot.py
@@ -0,0 +1,157 @@
+import os
+import csv
+import numpy as np
+import torch as th
+import gif
+import matplotlib
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import re
+
+if __name__ == "__main__":
+    matplotlib.use('Agg')
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', '-d', type=str, help='Directory with npz files')
+    parser.add_argument('--gif_duration', '-gif_d', type=int, default=30)
+    parser.add_argument('--grid_points', type=int, default=501)
+    parser.add_argument('--make_gif', '-gif', action='store_true', help="Create gif of plots")
+    parser.add_argument('--device', '-dev', type=str, default='cuda', help='cuda or cpu')
+    parser.add_argument('--vi_ent_samples', type=int, default=5,
+                        help="VI entropy approximation with these many samples")
+    parser.add_argument('--mc_ent_samples', type=int, default=100,
+                        help="Monte Carlo entropy approximation with these many samples")
+    args = parser.parse_args()
+
+    config = None
+    cwd = os.path.realpath(args.dir)
+    dir_name = os.path.split(cwd)[1]
+    for filename in os.listdir(cwd):
+        filename = os.fsdecode(filename)
+        if filename.endswith('.csv'):
+            with open(os.path.join(cwd, filename)) as f:
+                reader = csv.DictReader(f)
+                config = [r for r in reader][0]
+    assert config is not None, f"No config file was found in directory {args.dir}."
+    grid_points = args.grid_points
+    min_x = -float(config['max_abs_mean'])
+    max_x = -min_x
+
+    train_mode = None
+    if config['huber']:
+        train_mode = 'huber'
+    elif config['vi']:
+        train_mode = 'vi'
+    elif config['mc']:
+        train_mode = 'mc'
+    assert train_mode is not None, "No train mode was specified in the saved csv file!"
+
+    x = th.linspace(min_x, max_x, grid_points)
+    grid = th.stack(th.meshgrid((x, x), indexing='ij'), dim=-1)
+    grid = grid.reshape(-1, 2)
+    grid_tensor = th.as_tensor(grid, device=args.device, dtype=th.float)
+
+    save_path = os.path.join(args.dir, f"{dir_name}")
+    save_nr = 1
+    while os.path.exists(save_path):
+        save_path = os.path.join(args.dir, f"{dir_name}__{save_nr}")
+        save_nr += 1
+
+    frames = []
+    fps = 0
+    if args.make_gif:
+        fps = 5
+        gif_duration = args.gif_duration  # seconds
+        n_frames = fps * gif_duration
+        make_frame_every = int(config['steps']) / n_frames
+    else:
+        make_frame_every = 10  # 5000
+
+    def pass_grid_to_model(grid):
+        global model
+        grid = grid.unsqueeze(1).unsqueeze(-1).unsqueeze(-1)  # Add obligatory leaf output channel and repetition dims
+        return model(grid).squeeze(-1).squeeze(-1)
+
+    def scale_to_grid(t: np.ndarray):
+        return (t - min_x) / (max_x - min_x) * grid_points
+
+    def exp_view(t: np.ndarray):
+        return np.exp(t).reshape(grid_points, grid_points).transpose()
+
+    def dist_imshow(handle, probs, apply_exp_view=True, **kwargs):
+        # handle is either an axis or plt
+        if apply_exp_view:
+            probs = exp_view(probs)
+        handle.imshow(probs, extent=[min_x, max_x, max_x, min_x], **kwargs)
+
+    @gif.frame
+    def gif_frame(*args, **kwargs):
+        plot_dist(*args, **kwargs)
+
+    def plot_dist(probs, huber_ent, vi_ent, mc_ent, mpe, step, train_mode: str):
+        fig, (ax1) = plt.subplots(1, figsize=(10, 10), dpi=200)
+        ax1.imshow(exp_view(probs))
+        if mpe is not None:
+            mpe = mpe.reshape(-1, *mpe.shape[-2:])
+            mpe = scale_to_grid(mpe)
+            for rep in range(mpe.shape[-1]):
+                ax1.scatter(mpe[:, 0, rep], mpe[:, 1, rep], s=5, label=f"Modes of rep {rep}")
+                # [ax1.text(mpe[i, 0, rep], mpe[i, 1, rep], str(rep), ha="center", va="center") for i in range(mpe.shape[0])]
+                [ax1.annotate(str(rep), (mpe[i, 0, rep], mpe[i, 1, rep]), color='white') for i in range(mpe.shape[0])]
+
+            ax1.legend()
+        if train_mode == 'huber':
+            train_mode = 'Huber entropy LB'
+        elif train_mode == 'vi':
+            train_mode = 'VI entropy approximation'
+        elif train_mode == 'mc':
+            train_mode = 'MC entropy approximation'
+
+        fig.suptitle(f"RatSpn distribution at step {step}, trained with {train_mode}")
+        ax1.set_title(f"Huber ent. LB: {huber_ent:.2f} - VI ent. approx.: {vi_ent:.2f} - MC ent. approx.: {mc_ent:.2f}")
+
+    step_pattern = re.compile('step([0-9]+)')
+
+    prev_step = 0
+    for filename in tqdm(sorted(os.listdir(os.path.join(cwd, 'models'))), desc='Progress'):
+        filename = os.fsdecode(filename)
+        model = th.load(os.path.join(cwd, 'models', filename), map_location=args.device).eval()
+        grid = grid_tensor.clone()
+        if False:
+            model.log_stds = model.log_stds * 0
+            [model.debug__set_weights_uniform(i) for i in model.sum_layer_indices]
+            model.debug__set_dist_params(min_mean=min_x + 5, max_mean=max_x - 5)
+            # del model._leaf.base_leaf.permutation
+            # model._leaf.base_leaf.permutation = th.as_tensor([0, 2, 1, 3]).unsqueeze(1).repeat(1, 3)
+        with th.no_grad():
+            if model.config.F > 2:
+                pad = th.zeros(grid.size(0), model.config.F - 2, device=args.device) * th.nan
+                grid = th.cat((grid, pad), 1)
+            if False:
+                chunks = th.split(grid, 10000, 0)
+                probs = [model(t) for t in chunks]
+                probs = th.cat(probs, 0)
+            else:
+                probs = pass_grid_to_model(grid)
+
+            mpe = model.sample(mode='onehot', n=1, layer_index=1, is_mpe=True).sample
+            huber_ent, huber_log = model.huber_entropy_lb(verbose=True)
+            vi_ent, vi_log = model.vi_entropy_approx_layerwise(sample_size=args.vi_ent_samples, verbose=True)
+            mc_ent = model.monte_carlo_ent_approx(sample_size=args.mc_ent_samples)
+
+        step = step_pattern.search(filename)
+        assert step is not None, f"The model file name {filename} didn't contain a 'step[0-9]+' sequence!"
+        step = step.groups()[0]
+        plot_args = {
+            'probs': probs.cpu().numpy(), 'mpe': mpe.cpu().numpy(), 'step': step, 'train_mode': train_mode,
+            'huber_ent': huber_ent.item(), 'vi_ent': vi_ent.item(), 'mc_ent': mc_ent.item(),
+        }
+        if args.make_gif:
+            frame = gif_frame(**plot_args)
+            frames.append(frame)
+        elif True:
+            plot_dist(**plot_args)
+            plt.show()
+
+    if args.make_gif:
+        gif.save(frames, f"{save_path}.gif", duration=1 / fps, unit='s')
