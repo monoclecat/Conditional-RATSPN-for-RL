6128025cbdb098685ee3f2b12bc04bbac8d2e0e9
Andrew DeLay <git@andrewdelay.com>
2022-08-18 15:45:14 +0200

    Added test for RatSPN

--- tests/test_RatSpn.py
+++ tests/test_RatSpn.py
@@ -6,7 +6,7 @@ from distributions import *
 class RatSpnTest(unittest.TestCase):
     def setUp(self) -> None:
         config = RatSpnConfig()
-        config.C = 1#10
+        config.C = 5
         config.F = 64
         config.R = 10
         config.D = 3
@@ -16,6 +16,11 @@ class RatSpnTest(unittest.TestCase):
         config.leaf_base_class = RatNormal
         self.model = RatSpn(config)
         self.model.debug__set_dist_params(min_mean=-100.0, max_mean=100.0)
+
+    def test_onehot_and_index_mpe_equal(self):
+        onehot_mpe = self.model.sample(mode='onehot', is_mpe=True)
+        index_mpe = self.model.sample(mode='index', is_mpe=True)
+        assert (onehot_mpe.sample == index_mpe.sample).all()
 
     # def test_something(self):
         # sample1 = self.model.sample(mode='index', is_mpe=True, do_sample_postprocessing=True)
