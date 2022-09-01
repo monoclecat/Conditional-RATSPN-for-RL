151b1e7a86a32067e4563af757b4fdcdedb7f31c
Andrew DeLay <git@andrewdelay.com>
2022-09-01 11:26:09 +0200

    create_gif.py: Adapt dir arg for to accept many directories at once

--- experiments/create_gif.py
+++ experiments/create_gif.py
@@ -5,21 +5,23 @@ from PIL import Image
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dir', '-d', type=str, help='Directory with frames', required=True)
+    parser.add_argument('--dir', '-d', type=str, nargs='+', help='Directory with frames', required=True)
     parser.add_argument('--duration', '-dur', type=int, default=30, help="Duration of the gif or video in seconds.")
     args = parser.parse_args()
 
-    cwd = os.path.realpath(args.dir)
-    print(f"Reading from {cwd}")
-    # parent_dir, dir_name = os.path.split(cwd)
-    gif_save_path = cwd
+    for cwd in args.dir:
+        cwd = cwd.replace('\'', '')
+        cwd = os.path.realpath(cwd)
+        assert os.path.exists(cwd), f"Path {cwd} doesn't exist!"
+        print(f"Reading from {cwd}")
+        gif_save_path = cwd
 
-    print(f"Creating gif and saving it to {args.dir}")
-    frames = [Image.open(image) for image in sorted(glob.glob(f"{cwd}/*.jpg"))]
-    assert len(frames) > 0, f"No frames in frame directory {cwd}."
-    frames[0].save(f"{gif_save_path}.gif", format="GIF", append_images=frames, save_all=True,
-                   duration=args.duration * 1000 / len(frames), loop=0)
-    if False:
-        os.system(
-            f"ffmpeg -f image2 -r {args.duration} -i {cwd}/* "
-            f"-vcodec mpeg4 -y {gif_save_path}.mp4")
+        print(f"Creating gif and saving it to {cwd}")
+        frames = [Image.open(image) for image in sorted(glob.glob(f"{cwd}/*.jpg"))]
+        assert len(frames) > 0, f"No frames in frame directory {cwd}."
+        frames[0].save(f"{gif_save_path}.gif", format="GIF", append_images=frames, save_all=True,
+                       duration=args.duration * 1000 / len(frames), loop=0)
+        if False:
+            os.system(
+                f"ffmpeg -f image2 -r {args.duration} -i {cwd}/* "
+                f"-vcodec mpeg4 -y {gif_save_path}.mp4")
