9cc01fb12030583f23cc72ca4cf6a93520198fa3
Andrew DeLay <git@andrewdelay.com>
2022-09-16 10:42:14 +0200

    cspn_sac_sb3.py: Put experiment in its own function for cluster

--- experiments/cspn_sac_sb3.py
+++ experiments/cspn_sac_sb3.py
@@ -19,6 +19,214 @@ from sb3 import *
 from utils import non_existing_folder_name
 
 
+def joint_failure_sac(
+        seed: List[int],
+        mlp_actor: bool,
+        num_envs: int,
+        timesteps: int,
+        save_interval: int,
+        log_interval: int,
+        env_name: str,
+        device: str,
+        proj_name: str,
+        run_name: str,
+        log_dir: str,
+        load_model_path: Optional[str],
+        no_wandb: bool,
+        no_video: bool,
+        ent_coef: float,
+        learning_rate: float,
+        learning_starts: int,
+        buffer_size: int,
+        joint_fail_prob: float,
+        provide_joint_fail_info_to_critic: bool,
+        repetitions: int,
+        cspn_depth: int,
+        num_dist: int,
+        num_sums: int,
+        dropout: float,
+        feat_layers: Optional[List[int]],
+        sum_param_layers: Optional[List[int]],
+        dist_param_layers: Optional[List[int]],
+        objective: str,
+        recurs_sample_size: int,
+        naive_sample_size: int,
+):
+    if not save_interval:
+        save_interval = timesteps
+
+    assert os.environ.get('LD_PRELOAD') is None, "The LD_PRELOAD environment variable may not be set externally."
+    if no_video:
+        os.environ['LD_PRELOAD'] = os.environ.get('CONDA_PREFIX') + '/lib/libGLEW.so'
+
+    if timesteps == 0:
+        learn = False
+    else:
+        learn = True
+        assert timesteps >= save_interval, "Total timesteps cannot be lower than save_interval!"
+        assert timesteps % save_interval == 0, "save_interval must be a divisor of total timesteps."
+
+    if load_model_path:
+        assert os.path.exists(load_model_path), f"The model_path doesn't exist! {load_model_path}"
+    if log_dir:
+        assert os.path.exists(log_dir), f"The log_dir doesn't exist! {log_dir}"
+
+    if not no_wandb:
+        import wandb
+        from wandb.integration.sb3 import WandbCallback
+        wandb.login(key=os.environ['WANDB_API_KEY'])
+
+    for seed in seed:
+        print(f"Seed: {seed}")
+        np.random.seed(seed)
+        th.manual_seed(seed)
+
+        run_name = f"{'MLP' if mlp_actor else 'CSPN'}_{run_name}"
+        run_name_seed = f"{run_name}_s{seed}"
+        log_path = os.path.join(log_dir, proj_name)
+        log_path = os.path.join(log_path, non_existing_folder_name(log_path, run_name_seed))
+        monitor_path = os.path.join(log_path, "monitor")
+        model_path = os.path.join(log_path, "models")
+        video_path = os.path.join(log_path, "video")
+        for d in [log_path, monitor_path, model_path, video_path]:
+            os.makedirs(d, exist_ok=True)
+
+        if not no_wandb:
+            run = wandb.init(
+                dir=log_path,
+                project=proj_name,
+                name=run_name_seed,
+                group=run_name,
+                sync_tensorboard=True,
+                monitor_gym=True,
+                reinit=True,
+                force=True,
+                settings=wandb.Settings(start_method="fork"),
+            )
+
+        env = make_vec_env(
+            env_id=env_name,
+            n_envs=num_envs,
+            monitor_dir=monitor_path,
+            wrapper_class=FloatWrapper,
+            # vec_env_cls=SubprocVecEnv,
+            # vec_env_kwargs={'start_method': 'fork'},
+        )
+        if not no_video:
+            # Without env as a VecVideoRecorder we need the env var LD_PRELOAD=$CONDA_PREFIX/lib/libGLEW.so
+            env = VecVideoRecorder(env, video_folder=video_path,
+                                   record_video_trigger=lambda x: x % save_interval == 0,
+                                   video_length=200)
+
+        if load_model_path:
+            model = CspnSAC.load(load_model_path, env)
+            # model.tensorboard_log = None
+            # model.vi_aux_resp_grad_mode = vi_aux_resp_grad_mode
+            # model_name = f"sac_loadedpretrained_{env}_{proj_name}_{run_name_seed}"
+            model.seed = seed
+            model.learning_starts = learning_starts
+            # model.learning_rate = learning_rate
+            sac_kwargs = {
+                'env': model.env,
+                'seed': model.seed,
+                'verbose': model.verbose,
+                'ent_coef': model.ent_coef,
+                'learning_starts': model.learning_starts,
+                'device': model.device,
+                'learning_rate': model.learning_rate,
+                'policy_kwargs': {
+                    'cspn_args': {
+                        'R': model.actor.cspn.config.R,
+                        'D': model.actor.cspn.config.D,
+                        'I': model.actor.cspn.config.I,
+                        'S': model.actor.cspn.config.S,
+                        'dropout': model.actor.cspn.config.dropout,
+                        'sum_param_layers': model.actor.cspn.config.sum_param_layers,
+                        'dist_param_layers': model.actor.cspn.config.dist_param_layers,
+                        'cond_layers_inner_act': model.actor.cspn.config.cond_layers_inner_act,
+                        'vi_ent_approx_sample_size': model.actor.vi_ent_approx_sample_size,
+                    }
+                }
+            }
+        else:
+            sac_kwargs = {
+                'env': env,
+                'seed': seed,
+                'verbose': 2,
+                'ent_coef': ent_coef,
+                'learning_starts': learning_starts,
+                'device': device,
+                'learning_rate': learning_rate,
+                'buffer_size': buffer_size,
+            }
+            cspn_args = {
+                'R': repetitions,
+                'D': cspn_depth,
+                'I': num_dist,
+                'S': num_sums,
+                'dropout': dropout,
+                'feat_layers': feat_layers,
+                'sum_param_layers': sum_param_layers,
+                'dist_param_layers': dist_param_layers,
+                'cond_layers_inner_act': nn.LeakyReLU,  # nn.Identity if no_relu else nn.ReLU,
+                'entropy_objective': objective,
+                'recurs_ent_approx_sample_size': recurs_sample_size,
+                'naive_ent_approx_sample_size': naive_sample_size,
+            }
+            sac_kwargs['policy_kwargs'] = {
+                'joint_failure_prob': joint_fail_prob,
+                'sample_failing_joints': True,
+                'provide_joint_fail_info_to_critic': provide_joint_fail_info_to_critic,
+                'actor_cspn_args': cspn_args,
+                'features_extractor_class': NatureCNN if len(env.observation_space.shape) > 1 else FlattenExtractor,
+            }
+            model = CspnSAC(policy=CustomMlpPolicy if mlp_actor else CspnPolicy, **sac_kwargs)
+            # model_name = f"sac_{'mlp' if mlp_actor else 'cspn'}_{env_name}_{exp_name}_s{seed}"
+
+        callback = [CheckpointCallback(
+            save_freq=save_interval,
+            save_path=model_path,
+            name_prefix=run_name
+        )]
+        if not no_wandb:
+            log_args = vars(args).copy()
+            log_args_keys = list(log_args.keys())
+            for key in log_args_keys:
+                log_args[f'config/{key}'] = log_args.pop(key)
+            wandb.log(log_args)
+            run.config.update({
+                **sac_kwargs,
+                'machine': platform.node(),
+            })
+            callback.append(WandbCallback(
+                # gradient_save_freq=10000,
+                # model_save_path=model_path,
+                # model_save_freq=save_interval,
+                verbose=2,
+            ))
+
+        logger = configure(log_path, ["stdout", "csv", "tensorboard"])
+        logger.output_formats[0].max_length = 50
+        model.set_logger(logger)
+
+        print(model.actor)
+        print(model.critic)
+        if isinstance(model.actor, CspnActor):
+            print_cspn_params(model.actor.cspn)
+        else:
+            print(f"Actor MLP has {sum(p.numel() for p in model.actor.parameters() if p.requires_grad)} parameters.")
+
+        # noinspection PyTypeChecker
+        model.learn(
+            total_timesteps=timesteps,
+            log_interval=log_interval,
+            reset_num_timesteps=not model_path,
+            tb_log_name=f"{proj_name}/{run_name_seed}",
+            callback=callback,
+        )
+        run.finish()
+
+
 if __name__ == "__main__":
     import argparse
 
@@ -36,8 +244,7 @@ if __name__ == "__main__":
                         help='Name of this run for WandB. The seed will be automatically appended. ')
     parser.add_argument('--log_dir', type=str, default='../../cspn_rl_experiments',
                         help='Directory to save logs to.')
-    parser.add_argument('--model_path', type=str,
-                        help='Path to the pretrained model.')
+    parser.add_argument('--load_model_path', type=str, help='Path to the pretrained model.')
     parser.add_argument('--no_wandb', action='store_true', help="Don't log this run in WandB")
     parser.add_argument('--no_video', action='store_true', help="Don't record videos of the agent.")
     # SAC arguments
@@ -69,177 +276,4 @@ if __name__ == "__main__":
     parser.add_argument('--naive_sample_size', type=int, default=50,
                         help='Number of samples to approximate naive entropy with. ')
     args = parser.parse_args()
-
-    if not args.save_interval:
-        args.save_interval = args.timesteps
-
-    assert os.environ.get('LD_PRELOAD') is None, "The LD_PRELOAD environment variable may not be set externally."
-    if args.no_video:
-        os.environ['LD_PRELOAD'] = os.environ.get('CONDA_PREFIX') + '/lib/libGLEW.so'
-
-    if args.timesteps == 0:
-        learn = False
-    else:
-        learn = True
-        assert args.timesteps >= args.save_interval, "Total timesteps cannot be lower than save_interval!"
-        assert args.timesteps % args.save_interval == 0, "save_interval must be a divisor of total timesteps."
-
-    if args.model_path:
-        assert os.path.exists(args.model_path), f"The model_path doesn't exist! {args.model_path}"
-    if args.log_dir:
-        assert os.path.exists(args.log_dir), f"The log_dir doesn't exist! {args.log_dir}"
-
-    if not args.no_wandb:
-        import wandb
-        from wandb.integration.sb3 import WandbCallback
-        wandb.login(key=os.environ['WANDB_API_KEY'])
-
-    for seed in args.seed:
-        print(f"Seed: {seed}")
-        np.random.seed(seed)
-        th.manual_seed(seed)
-
-        run_name = f"{'MLP' if args.mlp_actor else 'CSPN'}_{args.run_name}"
-        run_name_seed = f"{run_name}_s{seed}"
-        log_path = os.path.join(args.log_dir, args.proj_name)
-        log_path = os.path.join(log_path, non_existing_folder_name(log_path, run_name_seed))
-        monitor_path = os.path.join(log_path, "monitor")
-        model_path = os.path.join(log_path, "models")
-        video_path = os.path.join(log_path, "video")
-        for d in [log_path, monitor_path, model_path, video_path]:
-            os.makedirs(d, exist_ok=True)
-
-        if not args.no_wandb:
-            run = wandb.init(
-                dir=log_path,
-                project=args.proj_name,
-                name=run_name_seed,
-                group=run_name,
-                sync_tensorboard=True,
-                monitor_gym=True,
-                reinit=True,
-                force=True,
-                settings=wandb.Settings(start_method="fork"),
-            )
-
-        env = make_vec_env(
-            env_id=args.env_name,
-            n_envs=args.num_envs,
-            monitor_dir=monitor_path,
-            wrapper_class=FloatWrapper,
-            # vec_env_cls=SubprocVecEnv,
-            # vec_env_kwargs={'start_method': 'fork'},
-        )
-        if not args.no_video:
-            # Without env as a VecVideoRecorder we need the env var LD_PRELOAD=$CONDA_PREFIX/lib/libGLEW.so
-            env = VecVideoRecorder(env, video_folder=video_path,
-                                   record_video_trigger=lambda x: x % args.save_interval == 0,
-                                   video_length=200)
-
-        if args.model_path:
-            model = CspnSAC.load(args.model_path, env)
-            # model.tensorboard_log = None
-            # model.vi_aux_resp_grad_mode = args.vi_aux_resp_grad_mode
-            # model_name = f"sac_loadedpretrained_{args.env}_{args.proj_name}_{run_name_seed}"
-            model.seed = seed
-            model.learning_starts = args.learning_starts
-            # model.learning_rate = args.learning_rate
-            sac_kwargs = {
-                'env': model.env,
-                'seed': model.seed,
-                'verbose': model.verbose,
-                'ent_coef': model.ent_coef,
-                'learning_starts': model.learning_starts,
-                'device': model.device,
-                'learning_rate': model.learning_rate,
-                'policy_kwargs': {
-                    'cspn_args': {
-                        'R': model.actor.cspn.config.R,
-                        'D': model.actor.cspn.config.D,
-                        'I': model.actor.cspn.config.I,
-                        'S': model.actor.cspn.config.S,
-                        'dropout': model.actor.cspn.config.dropout,
-                        'sum_param_layers': model.actor.cspn.config.sum_param_layers,
-                        'dist_param_layers': model.actor.cspn.config.dist_param_layers,
-                        'cond_layers_inner_act': model.actor.cspn.config.cond_layers_inner_act,
-                        'vi_ent_approx_sample_size': model.actor.vi_ent_approx_sample_size,
-                    }
-                }
-            }
-        else:
-            sac_kwargs = {
-                'env': env,
-                'seed': seed,
-                'verbose': 2,
-                'ent_coef': args.ent_coef,
-                'learning_starts': args.learning_starts,
-                'device': args.device,
-                'learning_rate': args.learning_rate,
-                'buffer_size': args.buffer_size,
-            }
-            cspn_args = {
-                'R': args.repetitions,
-                'D': args.cspn_depth,
-                'I': args.num_dist,
-                'S': args.num_sums,
-                'dropout': args.dropout,
-                'feat_layers': args.feat_layers,
-                'sum_param_layers': args.sum_param_layers,
-                'dist_param_layers': args.dist_param_layers,
-                'cond_layers_inner_act': nn.LeakyReLU,  # nn.Identity if args.no_relu else nn.ReLU,
-                'entropy_objective': args.objective,
-                'recurs_ent_approx_sample_size': args.recurs_sample_size,
-                'naive_ent_approx_sample_size': args.naive_sample_size,
-            }
-            sac_kwargs['policy_kwargs'] = {
-                'joint_failure_prob': args.joint_fail_prob,
-                'sample_failing_joints': True,
-                'provide_joint_fail_info_to_critic': args.provide_joint_fail_info_to_critic,
-                'actor_cspn_args': cspn_args,
-                'features_extractor_class': NatureCNN if len(env.observation_space.shape) > 1 else FlattenExtractor,
-            }
-            model = CspnSAC(policy=CustomMlpPolicy if args.mlp_actor else CspnPolicy, **sac_kwargs)
-            # model_name = f"sac_{'mlp' if args.mlp_actor else 'cspn'}_{args.env_name}_{args.exp_name}_s{seed}"
-
-        callback = [CheckpointCallback(
-            save_freq=args.save_interval,
-            save_path=model_path,
-            name_prefix=run_name
-        )]
-        if not args.no_wandb:
-            log_args = vars(args).copy()
-            log_args_keys = list(log_args.keys())
-            for key in log_args_keys:
-                log_args[f'config/{key}'] = log_args.pop(key)
-            wandb.log(log_args)
-            run.config.update({
-                **sac_kwargs,
-                'machine': platform.node(),
-            })
-            callback.append(WandbCallback(
-                # gradient_save_freq=10000,
-                # model_save_path=model_path,
-                # model_save_freq=args.save_interval,
-                verbose=2,
-            ))
-
-        logger = configure(log_path, ["stdout", "csv", "tensorboard"])
-        logger.output_formats[0].max_length = 50
-        model.set_logger(logger)
-
-        print(model.actor)
-        print(model.critic)
-        if isinstance(model.actor, CspnActor):
-            print_cspn_params(model.actor.cspn)
-        else:
-            print(f"Actor MLP has {sum(p.numel() for p in model.actor.parameters() if p.requires_grad)} parameters.")
-
-        # noinspection PyTypeChecker
-        model.learn(
-            total_timesteps=args.timesteps,
-            log_interval=args.log_interval,
-            reset_num_timesteps=not args.model_path,
-            tb_log_name=f"{args.proj_name}/{run_name_seed}",
-            callback=callback,
-        )
-        run.finish()
+    joint_failure_sac(**vars(args))
