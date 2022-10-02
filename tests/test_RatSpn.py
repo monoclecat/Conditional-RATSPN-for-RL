2dd7aba94c6c977c34338ddd33edac95428eee1f
monoclecat <git@andrewdelay.com>
2022-10-02 21:21:29 +0200

    Added test which fails as of now. Why?

--- tests/test_RatSpn.py
+++ tests/test_RatSpn.py
@@ -1,3 +1,4 @@
+import copy
 import unittest
 from rat_spn import *
 from distributions import *
@@ -21,6 +22,16 @@ class RatSpnTest(unittest.TestCase):
         onehot_mpe = self.model.sample(mode='onehot', is_mpe=True)
         index_mpe = self.model.sample(mode='index', is_mpe=True)
         assert (onehot_mpe.sample == index_mpe.sample).all()
+
+    def test_log_prob_of_own_samples_when_permuted_and_not_permuted(self):
+        ctx = self.model.sample(mode='index', do_sample_postprocessing=False)
+        not_inv_permuted = self.model.sample_postprocessing(copy.deepcopy(ctx), invert_permutation=False)
+        inv_permuted = self.model.sample_postprocessing(copy.deepcopy(ctx), invert_permutation=True)
+        not_inv_permuted.sample = th.einsum('o...r -> ...or', not_inv_permuted.sample.unsqueeze(-1))
+        log_prob_not_inv_permuted = self.model.forward(not_inv_permuted.sample, x_needs_permutation=False)
+        inv_permuted.sample = th.einsum('o...r -> ...or', inv_permuted.sample)
+        log_prob_inv_permuted = self.model.forward(inv_permuted.sample)
+        assert (log_prob_not_inv_permuted == log_prob_inv_permuted).all()
 
 
 if __name__ == '__main__':
