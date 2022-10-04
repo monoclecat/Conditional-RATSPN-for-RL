9d24158680bd56e6a5a3417f803528b757b207c3
Andrew DeLay <git@andrewdelay.com>
2022-10-04 16:29:10 +0200

    Modified entropy_density_plot.py to allow for differing ranges to evaluate probs over.

--- experiments/entropy_density_plot.py
+++ experiments/entropy_density_plot.py
@@ -7,12 +7,11 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import re
 from utils import non_existing_folder_name
-import wandb
 
 
 def get_ents_from_metrics(metrics, step):
     ents = []
-    for m_name in ['recursive_ent_approx', 'huber_entropy_LB', 'naive_root_entropy']:
+    for m_name in ['VI_ent_approx', 'huber_entropy_LB', 'MC_root_entropy']:
         try:
             ents.append(metrics.get(m_name)[step])
         except IndexError:
@@ -24,15 +23,15 @@ def get_ents_from_metrics(metrics, step):
     return ents
 
 
-def set_axis_ticks_and_labels(ax, grid_points, num_ticks, min_x, max_x, num_feat):
+def set_axis_ticks_and_labels(ax, grid_points, num_ticks, x_abs_max, num_feat):
     ax_ticks = np.linspace(0.0, grid_points, num_ticks)
-    ax_ticklabels = np.asarray(np.around(ax_ticks * ((max_x - min_x) / grid_points) + min_x, decimals=1), dtype='str')
+    ax_ticklabels = np.asarray(np.around(ax_ticks * (2 * x_abs_max / grid_points) - x_abs_max, decimals=1), dtype='str')
     ax.set_xticks(ax_ticks)
     ax.set_yticks(ax_ticks)
     ax.set_xticklabels(ax_ticklabels)
     ax.set_yticklabels(ax_ticklabels)
-    ax.set_xlabel(f"x (feature no. 0 of {num_feat})")
-    ax.set_ylabel(f"y (feature no. 1 of {num_feat})")
+    ax.set_xlabel(f"x")  # feature no. 0 of {num_feat}
+    ax.set_ylabel(f"y")  # feature no. 1 of {num_feat}
 
 
 if __name__ == "__main__":
@@ -40,15 +39,16 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', '-d', type=str, help='Directory with model files', required=True)
-    parser.add_argument('--grid_points', type=int, default=500)
     parser.add_argument('--vmin', type=float,
                         help="Maximum probability value on colorbar. If left out will default to the "
                              "max value among all probs during experiment.")
     parser.add_argument('--vmax', type=float,
                         help="Maximum probability value on colorbar. If left out will default to the "
                              "max value among all probs during experiment.")
+    parser.add_argument('--x_abs_max', type=int, required=True,
+                        help="Maximum absolute range of grid to evaluate probability on")
     parser.add_argument('--plot_in_log_space', '-logspace', action='store_true', help="Plot densities on log scale")
-    parser.add_argument('--wandb', action='store_true', help='Log with wandb.')
+    parser.add_argument('--no_colorbar', action='store_true', help='Log with wandb.')
     args = parser.parse_args()
     device = th.device('cpu')
 
@@ -69,7 +69,7 @@ if __name__ == "__main__":
             keys = [i for i in metrics]
             assert len(keys) == 1, f"There should only be one key, but there were {len(keys)}"
             metrics = metrics[keys[0]].tolist()
-        if filename.endswith('.npz') and filename.startswith('model_log_probs'):
+        if filename.endswith('.npz') and filename.startswith(f'model_log_probs_x_abs_max{args.x_abs_max:03}'):
             model_log_probs = np.load(os.path.join(cwd, filename))
             probs = model_log_probs['probs']
             steps = model_log_probs['steps']
@@ -77,17 +77,18 @@ if __name__ == "__main__":
     assert config is not None, f"No config file was found in directory {args.dir}."
     train_mode = config['objective']
     assert train_mode is not None, "No train mode was specified in the saved csv file!"
+    assert args.x_abs_max > 0.0
 
     def grid_view(t: np.ndarray):
         return t.reshape(grid_points, grid_points).transpose()
 
-    min_x = -float(config['max_abs_mean'])
+    min_x = -float(args.x_abs_max)
     max_x = -min_x
     if probs is not None:
-        print(f"Found saved probs, ignoring args.grid_points.")
+        print(f"Found saved probs. Ignoring x_abs_max.")
         grid_points = probs.shape[-1]
     else:
-        grid_points = args.grid_points
+        grid_points = 500
         x = th.linspace(min_x, max_x, grid_points)
         grid = th.stack(th.meshgrid((x, x), indexing='ij'), dim=-1)
         grid = grid.reshape(-1, 2)
@@ -103,6 +104,7 @@ if __name__ == "__main__":
         for filename in tqdm(sorted(os.listdir(os.path.join(cwd, 'models'))), desc='Computing model probs'):
             filename = os.fsdecode(filename)
             model = th.load(os.path.join(cwd, 'models', filename), map_location=device).eval()
+            setattr(model._leaf.base_leaf, '_means_sigmoid_bound', False)
             grid = grid_tensor.clone()
             if False:
                 model.log_stds = model.log_stds * 0
@@ -119,8 +121,8 @@ if __name__ == "__main__":
                     curr_probs = [model(t) for t in chunks]
                     curr_probs = th.cat(curr_probs, 0)
                 else:
-                    curr_probs: th.Tensor = pass_grid_to_model(grid)
-                curr_probs = grid_view(curr_probs.cpu().numpy())
+                    curr_probs = pass_grid_to_model(grid)
+                curr_probs = curr_probs.cpu().numpy().reshape(grid_points, grid_points).transpose()
             curr_probs = np.expand_dims(np.asarray(curr_probs, dtype='f4'), 0)
             if probs is None:
                 probs = curr_probs
@@ -135,8 +137,8 @@ if __name__ == "__main__":
                 steps = curr_step
             else:
                 steps = np.concatenate((steps, curr_step), 0)
-        np.savez(os.path.join(cwd, 'model_log_probs.npz'), probs=probs, steps=steps)
-        model_log_probs = np.load(os.path.join(cwd, 'model_log_probs.npz'))
+        np.savez(os.path.join(cwd, f'model_log_probs_x_abs_max{args.x_abs_max:03}.npz'), probs=probs, steps=steps)
+        model_log_probs = np.load(os.path.join(cwd, f'model_log_probs_x_abs_max{args.x_abs_max:03}.npz'))
         probs = model_log_probs['probs']
         steps = model_log_probs['steps']
 
@@ -169,33 +171,26 @@ if __name__ == "__main__":
     else:
         args.vmin = abs(0.0 if args.vmin < 0.0 else args.vmin)
 
-    wandb_run = None
-    if args.wandb:
-        wandb.login(key=os.environ['WANDB_API_KEY'])
-        wandb_run = wandb.init(
-            resume='must',
-            id=config['wandb_run_id'],
-            project=config['proj_name'],
-            dir=args.dir,
-        )
-
-    frame_save_path = f"{'log' if args.plot_in_log_space else 'lin'}prob_vmin{args.vmin:.4f}_vmax{args.vmax:.4f}_{dir_name}"
+    frame_save_path = f"{'log' if args.plot_in_log_space else 'lin'}prob_xabsmax{args.x_abs_max}" \
+                      f"_vmin{args.vmin:.4f}" \
+                      f"_vmax{args.vmax:.4f}_{'no_colorbar' if args.no_colorbar else ''}_{dir_name}"
     frame_save_path = os.path.join(args.dir, non_existing_folder_name(args.dir, frame_save_path))
 
     def scale_to_grid(t: np.ndarray):
         return (t - min_x) / (max_x - min_x) * grid_points
 
-    def plot_dist(probs, huber_ent, vi_ent, mc_ent, mpe, step, train_mode: str):
+    def plot_dist(probs, huber_ent, vi_ent, mc_ent, mpe, step, train_mode: str, no_colorbar: bool, x_abs_max: int):
         fig, (ax1) = plt.subplots(1, figsize=(10, 10), dpi=200)
         norm = mpl.colors.Normalize(vmin=args.vmin, vmax=args.vmax, clip=True)
         cmap = mpl.cm.get_cmap('viridis')
-        ax1.imshow(grid_view(probs), norm=norm, cmap=cmap)
-        set_axis_ticks_and_labels(ax=ax1, grid_points=grid_points, num_ticks=6,
-                                  min_x=min_x, max_x=max_x, num_feat=config['RATSPN_F'])
+        ax1.imshow(probs.transpose(), norm=norm, cmap=cmap)
+        set_axis_ticks_and_labels(ax=ax1, grid_points=probs.shape[-1], num_ticks=6,
+                                  x_abs_max=x_abs_max, num_feat=config['RATSPN_F'])
 
-        cbar = fig.colorbar(mappable=mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax1)
-        cbar.ax.get_yaxis().labelpad = 15
-        cbar.ax.set_ylabel(f"Probability density [{'log scale' if args.plot_in_log_space else 'linear scale'}]", rotation=270)
+        if not no_colorbar:
+            cbar = fig.colorbar(mappable=mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax1)
+            cbar.ax.get_yaxis().labelpad = 15
+            cbar.ax.set_ylabel(f"Probability density [{'log scale' if args.plot_in_log_space else 'linear scale'}]", rotation=270)
         if mpe is not None:
             mpe = mpe.reshape(-1, *mpe.shape[-2:])
             mpe = scale_to_grid(mpe)
@@ -207,16 +202,19 @@ if __name__ == "__main__":
             ax1.legend()
         if train_mode == 'huber':
             train_mode = 'Huber entropy LB'
+        elif train_mode == 'huber_hack':
+            train_mode = 'augmented Huber entropy LB'
         elif train_mode == 'vi':
-            train_mode = 'VI entropy approximation'
+            train_mode = 'Recursive entropy approximation'
         elif train_mode == 'mc':
-            train_mode = 'MC entropy approximation'
+            train_mode = 'Naive entropy approximation'
 
-        fig.suptitle(f"RatSpn distribution at step {step}, trained with {train_mode}")
-        huber_ent_txt = f"Huber ent. LB: {huber_ent:.2f}" if huber_ent is not None else ''
-        vi_ent_txt = f"VI ent. approx.: {vi_ent:.2f}" if vi_ent is not None else ''
-        mc_ent_txt = f"MC ent. approx.: {mc_ent:.2f}" if mc_ent is not None else ''
-        ax1.set_title(' - '.join([huber_ent_txt, vi_ent_txt, mc_ent_txt]))
+        ax1.set_title(f"RatSpn distribution at step {step}, trained with {train_mode}")
+        if False:
+            huber_ent_txt = f"Huber ent. LB: {huber_ent:.2f}" if huber_ent is not None else ''
+            vi_ent_txt = f"VI ent. approx.: {vi_ent:.2f}" if vi_ent is not None else ''
+            mc_ent_txt = f"MC ent. approx.: {mc_ent:.2f}" if mc_ent is not None else ''
+            ax1.set_title(' - '.join([huber_ent_txt, vi_ent_txt, mc_ent_txt]))
         return fig
 
     if not args.plot_in_log_space:
@@ -232,7 +230,8 @@ if __name__ == "__main__":
 
         plot_args = {
             'probs': probs[i], 'mpe': mpe, 'step': steps[i], 'train_mode': train_mode,
-            'huber_ent': huber_ent, 'vi_ent': vi_ent, 'mc_ent': mc_ent,
+            'huber_ent': huber_ent, 'vi_ent': vi_ent, 'mc_ent': mc_ent, 'no_colorbar': args.no_colorbar,
+            'x_abs_max': args.x_abs_max,
         }
         fig = plot_dist(**plot_args)
         if not os.path.exists(frame_save_path):
